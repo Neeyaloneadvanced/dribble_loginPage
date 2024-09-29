efea62d07ad5e92f8857e4f8d451f2d927686fea
Neeyaloneadvanced <90691537+Neeyaloneadvanced@users.noreply.github.com>
2024-09-29 14:55:51 +0530

    fix: add animation and update the code for loading the page

--- ashu/index.js
+++ ashu/index.js
@@ -1,17 +1,44 @@
-const passwordInput = document.getElementById('password');
-const showButton = document.createElement('span');
-showButton.innerHTML = 'Show';
-showButton.style.cursor = 'pointer';
-showButton.style.color = '#4CAF50';
-showButton.style.marginLeft = '10px';
+document.addEventListener('DOMContentLoaded', function () {
+    const passwordInput = document.getElementById('password');
+    const showButton = document.createElement('span');
+    showButton.innerHTML = 'Show';
+    showButton.style.cursor = 'pointer';
+    showButton.style.color = '#4CAF50';
+    showButton.style.marginLeft = '10px';
 
-passwordInput.parentNode.appendChild(showButton);
-passwordInput.style.paddingRight = '50px';
-showButton.style.position = 'absolute';
-showButton.style.right = '10px';
-showButton.style.top = '50%';
-showButton.style.transform = 'translateY(-50%)';
-passwordInput.parentNode.style.position = 'relative';
+    passwordInput.parentNode.appendChild(showButton);
+    passwordInput.style.paddingRight = '50px';
+    showButton.style.position = 'absolute';
+    showButton.style.right = '10px';
+    showButton.style.top = '50%';
+    showButton.style.transform = 'translateY(-50%)';
+    passwordInput.parentNode.style.position = 'relative';
+
+    showButton.addEventListener('click', () => {
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            showButton.innerHTML = 'Hide';
+        } else {
+            passwordInput.type = 'password';
+            showButton.innerHTML = 'Show';
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const logoSplash = document.getElementById('logoSplash');
+    const mainContent = document.getElementById('mainContent');
+    mainContent.classList.add('hidden'); // Hide the main content
+
+
+    // Show the main content after 5 seconds
+    setTimeout(() => {
+        logoSplash.style.display = 'none';
+        mainContent.classList.remove('hidden') // Hide the splash screen
+        mainContent.style.display = 'flex'; // Show the main content
+    }, 5000);
+});
 
 showButton.addEventListener('click', () => {
     if (passwordInput.type === 'password') {
